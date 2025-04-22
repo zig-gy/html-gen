596f7c1225d290d1100f4f95b272fae0ba34acdf
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-22 14:13:55 -0400

    added unit tests for textnodes

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_dif_text_type(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_dif_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("this is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_link(self):
+        node = TextNode("This is a link", TextType.LINK, "www.hola.com")
+        node2 = TextNode("This is a link", TextType.LINK, "www.hola.com")
+        self.assertEqual(node, node2)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
