e8dd15fb979718098cdf5e2f0697fa896c6bdbdb
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-24 10:29:50 -0400

    added tests to inline and fixed bug with empty string

--- src/inline.py
+++ src/inline.py
@@ -5,7 +5,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text.count(delimiter) % 2 != 0:
             raise Exception("delimiter not closed")
-        if node.text_type != TextType.TEXT:
+        if node.text_type != TextType.TEXT or len(node.text) == 0:
             new_nodes.append(node)
         else:
             new_texts = node.text.split(delimiter)
--- src/test_inline.py
+++ src/test_inline.py
@@ -29,3 +29,64 @@ class TestInline(unittest.TestCase):
             TextNode("cosaFea()", TextType.CODE),
             TextNode(" Fin codigo", TextType.TEXT)
         ])
+        
+    def test_two_bold(self):
+        node = TextNode("**Bold 1** y tambien **Bold 2**", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("Bold 1", TextType.BOLD),
+            TextNode(" y tambien ", TextType.TEXT),
+            TextNode("Bold 2", TextType.BOLD)
+        ])
+        
+    def test_list_of_text(self):
+        node = TextNode("Hola buenas _tardes_ muchachos, ¿como estan?", TextType.TEXT)
+        node2 = TextNode("Buenas _tardes_ señorita.", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node, node2], '_', TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("Hola buenas ", TextType.TEXT),
+            TextNode("tardes", TextType.ITALIC),
+            TextNode(" muchachos, ¿como estan?", TextType.TEXT),
+            TextNode("Buenas ", TextType.TEXT),
+            TextNode("tardes", TextType.ITALIC),
+            TextNode(" señorita.", TextType.TEXT)
+        ])
+        
+    def test_non_closing_delimiter(self):
+        node = TextNode("Esto **no", TextType.TEXT)
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(str(context.exception), "delimiter not closed")
+            
+    def test_mixed_delimiters(self):
+        node = TextNode("Hola **mundo** _lindo_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, '_', TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("Hola ", TextType.TEXT),
+            TextNode("mundo", TextType.BOLD),
+            TextNode(" ", TextType.TEXT),
+            TextNode("lindo", TextType.ITALIC),
+        ])
+        
+    def test_empty_text(self):
+        node = TextNode("", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(new_nodes, [node])
+        
+    def test_no_delimiters(self):
+        node = TextNode("Texto sin delimitadores", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], '**', TextType.BOLD)
+        self.assertEqual(new_nodes, [node])
+        
+    def test_multiple_nodes_with_mixed_delimiters(self):
+        node1 = TextNode("Hola **mundo**", TextType.TEXT)
+        node2 = TextNode("_lindo_ dia", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node1, node2], '**', TextType.BOLD)
+        new_nodes = split_nodes_delimiter(new_nodes, '_', TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("Hola ", TextType.TEXT),
+            TextNode("mundo", TextType.BOLD),
+            TextNode("lindo", TextType.ITALIC),
+            TextNode(" dia", TextType.TEXT),
+        ])
