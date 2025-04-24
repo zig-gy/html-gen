9e65dee9f64554da05b8fb0b58b71d5a696755dc
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-24 11:21:04 -0400

    refactored split_nodes_delimiter function to be more readable

--- src/inline.py
+++ src/inline.py
@@ -7,19 +7,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             raise Exception("delimiter not closed")
         if node.text_type != TextType.TEXT or len(node.text) == 0:
             new_nodes.append(node)
-        else:
-            new_texts = node.text.split(delimiter)
-            if node.text[-1] == delimiter[0]:
-                new_texts = new_texts[:-1]
-            if node.text[0] == delimiter[0]:
-                new_texts = new_texts[1:]
-            for i, text in enumerate(new_texts):
-                check_even = 0
-                if node.text[0] != delimiter[0]:
-                    check_even = 1
-                    
-                if i % 2 == check_even:
-                    new_nodes.append(TextNode(text=text, text_type=text_type))
-                else:
-                    new_nodes.append(TextNode(text=text, text_type=TextType.TEXT))
+            continue
+        new_texts = node.text.split(delimiter)
+        for i, text in enumerate(new_texts):
+            if text == "":
+                continue
+            check_uneven = 1
+            if i % 2 == check_uneven:
+                new_nodes.append(TextNode(text=text, text_type=text_type))
+            else:
+                new_nodes.append(TextNode(text=text, text_type=TextType.TEXT))
     return new_nodes
