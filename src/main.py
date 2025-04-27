50a1f8c99155b1df9468f8ba4811c9a7acdaf32f
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-27 19:51:46 -0400

    added generation of the public directory and assets needed for it

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 def main():
-    test = TextNode("Test",TextType.NORMAL)
-    print(test)
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    copy_tree("static", "public")
+
+def copy_tree(src, dst):
+    os.mkdir(dst)
+    dirs = os.listdir(src)
+    for directory_name in dirs:
+        directory = os.path.join(src, directory_name)
+        dst_directory = os.path.join(dst, directory_name)
+        if os.path.isfile(directory):
+            shutil.copy(directory, dst)
+            return
+        copy_tree(directory, dst_directory)
 
 if __name__ == "__main__":
     main()
-
