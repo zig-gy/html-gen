47a66a63818c448a26cab5ef51ea08cbb4f190b2
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-28 11:42:51 -0400

    added generate page recursively for creating an html file for every md file in content

--- src/main.py
+++ src/main.py
@@ -5,10 +5,23 @@ from block import markdown_to_html_node, extract_title
 from parentnode import ParentNode
 
 def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    copy_tree("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    dst = "public"
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    copy_tree("static", dst)
+    generate_page_recursively("content", "template.html", dst)
+
+def generate_page_recursively(dir_path_content, template_path, dest_dir_path):
+    dirs = os.listdir(dir_path_content)
+    for directory_name in dirs:
+        directory = os.path.join(dir_path_content, directory_name)
+        dst_directory = os.path.join(dest_dir_path, directory_name)
+        if os.path.isfile(directory) and directory_name[-3:] == ".md":
+            dst_directory = dst_directory[:-3] + ".html"
+            generate_page(directory, template_path, dst_directory)
+            continue
+        generate_page_recursively(directory, template_path, dst_directory)
+    return
 
 def copy_tree(src, dst):
     os.mkdir(dst)
@@ -18,8 +31,9 @@ def copy_tree(src, dst):
         dst_directory = os.path.join(dst, directory_name)
         if os.path.isfile(directory):
             shutil.copy(directory, dst)
-            return
+            continue
         copy_tree(directory, dst_directory)
+    return
 
 def generate_page(src_path, template_path, dst_path):
     print(f"Generating page from {src_path} to {dst_path} using {template_path}")
@@ -40,7 +54,10 @@ def generate_page(src_path, template_path, dst_path):
     # print(output)
     if os.path.exists(dst_path):
         os.remove(dst_path)
-    # os.makedirs(dst_path)
+    paths = dst_path.split("/")
+    if len(paths) > 2:
+        make_paths = "/".join(paths[:-1])
+        os.makedirs(make_paths)
     with open(dst_path, "w") as f:
         f.write(output)
         f.close()
