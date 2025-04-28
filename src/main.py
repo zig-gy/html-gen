f41628b1bbcdf4ab34e26f06a1c9fc886085c680
zig-gy <benjamin.f.chadwick@proton.me>
2025-04-28 11:58:54 -0400

    added build script and docs folder for deploy with github pages

--- src/main.py
+++ src/main.py
@@ -1,26 +1,33 @@
 import os
 import shutil
+import sys
 
 from block import markdown_to_html_node, extract_title
 from parentnode import ParentNode
 
 def main():
+    root_path = "/"
     dst = "public"
+    # print(sys.argv)
+    if len(sys.argv) > 1:
+        print("passed")
+        dst = "docs"
+        root_path = sys.argv[1]
     if os.path.exists(dst):
         shutil.rmtree(dst)
     copy_tree("static", dst)
-    generate_page_recursively("content", "template.html", dst)
+    generate_page_recursively("content", "template.html", dst, root_path)
 
-def generate_page_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursively(dir_path_content, template_path, dest_dir_path, basepath):
     dirs = os.listdir(dir_path_content)
     for directory_name in dirs:
         directory = os.path.join(dir_path_content, directory_name)
         dst_directory = os.path.join(dest_dir_path, directory_name)
         if os.path.isfile(directory) and directory_name[-3:] == ".md":
             dst_directory = dst_directory[:-3] + ".html"
-            generate_page(directory, template_path, dst_directory)
+            generate_page(directory, template_path, dst_directory, basepath)
             continue
-        generate_page_recursively(directory, template_path, dst_directory)
+        generate_page_recursively(directory, template_path, dst_directory, basepath)
     return
 
 def copy_tree(src, dst):
@@ -35,7 +42,7 @@ def copy_tree(src, dst):
         copy_tree(directory, dst_directory)
     return
 
-def generate_page(src_path, template_path, dst_path):
+def generate_page(src_path, template_path, dst_path, basepath):
     print(f"Generating page from {src_path} to {dst_path} using {template_path}")
     markdown = ""
     template = ""
@@ -51,6 +58,8 @@ def generate_page(src_path, template_path, dst_path):
     # print(content)
     output = template.replace("{{ Title }}", title)
     output = output.replace("{{ Content }}", content)
+    output = output.replace('href="/', f'href="{basepath}')
+    output = output.replace('src="/', f'src="{basepath}')
     # print(output)
     if os.path.exists(dst_path):
         os.remove(dst_path)
